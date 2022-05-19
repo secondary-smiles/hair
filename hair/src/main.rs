b8e6dfb5f4af1306b6b250a0f3f6b0169ff62d4a
secondary-smiles <shavkinder@gmail.com>
2022-05-18 23:17:47 -0400

    Fix broken merge
    Listen, I'm new to git, I still need to learn how to merge properly

---
+++ hair/src/main.rs
@@ -0,0 +1,29 @@
+// Other files
+mod tcp_lib;
+mod parse;
+mod struct_lib;
+
+// Local dependencies
+use parse::{ parse_args };
+use struct_lib::{ Request };
+use tcp_lib::{parse_request, send_request_and_recv, connect_stream };
+
+
+// Dependencies
+use std::io::prelude::*;
+use std::env::args;
+
+fn main() {
+    let request: Request = parse_args(args().collect()).unwrap();
+
+    let mut stream = connect_stream(&request.url.host);
+
+    let raw_data = send_request_and_recv(&mut stream, &request);
+    let response = parse_request(raw_data);
+
+    println!("{}{}", response.headers, response.body);
+
+    stream.flush().unwrap();
+    // We don't need this yet because with http/1.0 the connection is closed automatically
+    //stream.shutdown(Shutdown::Both).expect("Shutdown call failed");
+}
---
+++ hair/src/struct_lib.rs
@@ -0,0 +1,30 @@
+pub struct Url {
+    pub host: String,
+    pub path: String,
+}
+
+pub struct Response {
+    pub headers: String,
+    pub body: String,
+}
+
+pub struct Request {
+    pub method: Option<String>,
+    pub url: Url,
+}
+
+impl std::fmt::Debug for Url {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Url {{ host: {}, path: {} }}", self.host, self.path)
+    }
+}
+
+impl std::fmt::Debug for Request {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "Request {{ method: {:?}, url: {:?} }}",
+            self.method, self.url
+        )
+    }
+}
